ae4234525071a57d0d07053aee00bcbd1182828e
The-Genium007 <lucas.giza@outlook.fr>
2025-11-05 16:15:37 +0100

    update: translate navigation and site data

--- data/navigation.js
+++ data/navigation.js
@@ -1,7 +1,7 @@
 export const navigation = [
-  { label: 'Accueil', href: '/#home' },
-  { label: 'À propos', href: '/#about' },
-  { label: 'Expérience', href: '/#experience' },
+  { label: 'Home', href: '/#home' },
+  { label: 'About', href: '/#about' },
+  { label: 'Experience', href: '/#experience' },
   { label: 'Skills', href: '/#skills' },
-  { label: 'Projets', href: '/#projects' },
+  { label: 'Projects', href: '/#projects' },
 ];
--- data/site.js
+++ data/site.js
@@ -1,7 +1,7 @@
 export const site = {
   name: 'Lucas GIZA',
-  title: 'Full Stack Developer',
-  description: 'Full-stack developer combining design, code, and DevOps to deliver scalable, intuitive web experiences.',
+  title: 'Front-end Developer',
+  description: 'Front-end developer combining design and code to deliver scalable, intuitive web experiences.',
   locale: 'en',
   url: 'https://lucasgiza.com',
 };
